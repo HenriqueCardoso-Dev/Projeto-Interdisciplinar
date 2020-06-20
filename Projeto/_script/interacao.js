841c2ad4e1ea0d6045a1d896afa16972c233d525
HenriqueCardoso-Dev <carloshenriqueramoscardoso@gmail.com>
2020-06-20 02:42:17 -0300

    7.1

--- Projeto/_script/interacao.js
+++ Projeto/_script/interacao.js
@@ -1,5 +1,5 @@
 // Coleta de input's:
-const nameValue = document.querySelector("input[name=nome]");
+const nameInput = document.querySelector("input[name=nome]");
 
 const emailInput = document.querySelector("input[name=email]");
 
@@ -7,11 +7,14 @@ const tellInput = document.querySelector("input[name=telefone]");
 
 const msgTextArea = document.querySelector("input[name=msgTxt]");
 
-//Coleta de valores vindos dos input's:
+const nSerieInput = document.querySelector("input[name=serie]");
 
+const gbSizeInput = document.querySelector("input[name=hd]");
+
+const imgInput = document.querySelector("input[name=arquivos");
 
 
 const buttonSend = document.querySelector("input[name=buttonS]");
     buttonSend.addEventListener("click", function msgReturn(){
-        alert(`Muito obrigado ${nameValue.value}, suas informações foram enviadas com sucesso!\n\nEm breve retronaremos com o orçamento em seu e-mail.`);
+        alert(`Muito obrigado ${nameInput.value}, suas informações foram enviadas com sucesso!\n\nEm breve retronaremos com o orçamento em seu e-mail.`);
     });
