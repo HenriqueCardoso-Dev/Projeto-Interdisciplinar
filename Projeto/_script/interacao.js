ee832222fc9543863eca6f1cbf95d7ee1ef84e3f
HenriqueCardoso-Dev <carloshenriqueramoscardoso@gmail.com>
2020-06-23 01:15:09 -0300

    7.4
    atualização básica em css, e alguns ajustes no javascript (ainda não exibe o alert).

--- Projeto/_script/interacao.js
+++ Projeto/_script/interacao.js
@@ -13,8 +13,12 @@ const gbSizeInput = document.querySelector("input[name=hd]");
 
 const imgInput = document.querySelector("input[name=arquivos");
 
+//criando a lista para depois coneguir pegar e xibir apenas o primeiro nome do usuário:
+const listName = nameInput.value.split(" ");
 
+
+//Retorno ao usuário após envio do formulário
 const buttonSend = document.querySelector("button[name=buttonS]");
-    buttonSend.addEventListener("click", function msgReturn(){
-        alert(`Muito obrigado ${nameInput.value}, suas informações foram enviadas com sucesso!\n\nEm breve retronaremos com o orçamento em seu e-mail.`);
+    buttonSend.addEventListener("click", function msgReturn() {
+        alert(`Muito obrigado ${listName[0]}, suas informações foram enviadas com sucesso!\n\nEm breve retronaremos com o orçamento em seu e-mail.`);
     });
