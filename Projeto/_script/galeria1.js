e0ce66b04c5d84fcb6e2006998433ca951d24d4a
HenriqueCardoso-Dev <carloshenriqueramoscardoso@gmail.com>
2020-07-09 12:53:56 -0300

    8.3
    ajuste do JS.

--- Projeto/_script/galeria1.js
+++ Projeto/_script/galeria1.js
@@ -1,11 +1,11 @@
-let imagens= document.querySelectorAll('img[class=vendas]');
-let modal= document.querySelector('.modal');
-let modalImg = document.querySelector('#modal_img');
-let modalImg = document.querySelector('bt_close')
-let srcVal="";
+let imagens = document.querySelectorAll('img.vendas');
+let modal = document.querySelector('div.modal');
+let modalImg = document.querySelector('img#modal_img');
+let btnclosed = document.querySelector('span#bt_close')
+let srcVal =  "";
 
-for(let i = 0; i<imagens.length;i++){
-    imagens[i].addEventListener('click', function(){
-        alert(i)
+for(let i = 0; i < imagens.length;i++){
+    imagens[i].addEventListener('click', function () {
+        console.log(i);
     });
 }
