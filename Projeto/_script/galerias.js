9a92e40992ada676091a69659f39baf3fa89a093
Carlos Henrique Ramos Cardoso <63278045+HenriqueCardoso-Dev@users.noreply.github.com>
2020-07-15 16:19:14 -0300

    8.5

--- Projeto/_script/galerias.js
+++ Projeto/_script/galerias.js
@@ -1,9 +1,9 @@
 //Declaração de variáveis
 let imagens = document.querySelectorAll('img.vendas');
+let imagensManut = document.querySelectorAll('img.manutencao');
 let modal = document.querySelector('div.modal');
 let modalImg = document.querySelector('img#modal_img');
 let btnclosed = document.querySelector('span#bt_close');
-
 
 //codigicação da galeria Vendas
 for(let i = 0; i < imagens.length;i++){
@@ -17,59 +17,157 @@ for(let i = 0; i < imagens.length;i++){
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto1.jpg" id="modal_img">';
                 document.querySelector('img#modal_img').style.width = '35%';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 1 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto2.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 2 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto3.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 3 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto4.jpg" id="modal_img">';
                 document.querySelector('img#modal_img').style.width = '35%';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 4 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto5.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 5 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto6.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 6 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto7.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 7 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto8.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 8 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto9.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
 
             case 9 :
                 document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto10.jpg" id="modal_img">';
                 document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
             break;
         };    
+
+        
         
         //dando funcionalidade ao botão "X".
         btnclosed.addEventListener('click', function fechar() {
 
             document.querySelector('div.modal').style.visibility = 'hidden';
+            document.querySelector('footer').style.position = 'relative';
+
+        });
+
+
+    });
+}
+
+//codigicação da galeria Manutenção
+for(let i = 0; i < imagensManut.length;i++){
+    imagensManut[i].addEventListener('click', function () {
+        console.log(i);
+
+        //ações a serem feitas e acordo com a navegação do usuário
+        switch (i) {
+
+            case 0 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto1-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 1 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto2-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('img#modal_img').style.width = '40%';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 2 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto3-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 3 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto4-manutencao.jpg" id="modal_img">';
+                document.querySelector('img#modal_img').style.width = '50%';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 4 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto5-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 5 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto6-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 6 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto7-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 7 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto8-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 8 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto9-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+
+            case 9 :
+                document.querySelector('div.modal_content').innerHTML = '<img src="../_media/_photos/foto10-manutencao.jpg" id="modal_img">';
+                document.querySelector('div.modal').style.visibility = 'visible';
+                document.querySelector('footer').style.position = 'fixed';
+            break;
+        };    
+
+        
+        
+        //dando funcionalidade ao botão "X".
+        btnclosed.addEventListener('click', function fechar() {
+
+            document.querySelector('div.modal').style.visibility = 'hidden';
+            document.querySelector('footer').style.position = 'relative';
 
         });
 
